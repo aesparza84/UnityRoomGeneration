b8e689fa3e8380c4e573b8ab20c7006198ad59c2
aesparza84 <aesparza@colum.edu>
2023-09-16 23:26:22 -0500

    uncontrolled generation

---
+++ Project/ProceduralRoomsPOC/Assets/RoomsToAdd.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomsToAdd : MonoBehaviour
+{
+    public GameObject[] NorthRooms;
+    public GameObject[] EastRooms;
+    public GameObject[] SouthRooms;
+    public GameObject[] WestRooms;
+    public GameObject FourDoorRoom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Project/ProceduralRoomsPOC/Assets/Scripts/RoomSpawner.cs
+++ Project/ProceduralRoomsPOC/Assets/Scripts/RoomSpawner.cs
@@ -5,15 +5,50 @@ using UnityEngine;
 public class RoomSpawner : MonoBehaviour
 {
     public Transform entryN, entryE, entryS, entryW;
-    // Start is called before the first frame update
+    public bool hasN, hasE, hasS, hasW;
+
+    private RoomsToAdd possibleRooms;
+
     void Start()
     {
-        
+        possibleRooms = GameObject.FindGameObjectWithTag("RoomManager").GetComponent<RoomsToAdd>();
+        checkOpoenings();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        Debug.Log("Has N: " + hasN);
+        Debug.Log("Has E: " + hasE);
+        Debug.Log("Has S: " + hasS);
+        Debug.Log("Has W: " + hasW);
+    }
+
+    private void checkOpoenings()
+    {
+        if (entryN != null)
+        {
+            hasN = true;
+            
+            GameObject n = Instantiate(possibleRooms.NorthRooms[getRandomElement(possibleRooms.NorthRooms)], entryN.position, Quaternion.identity);
+        }
+        if (entryE != null)
+        {
+            hasE = true;
+        }
+        if (entryS != null)
+        {
+            hasS = true;
+        }
+        if (entryW != null)
+        {
+            hasW = true;
+        }
+    }
+
+    private int getRandomElement(GameObject[] n)
+    { 
+        int r = Random.Range(0, n.Length);
+        return r;
     }
 }
