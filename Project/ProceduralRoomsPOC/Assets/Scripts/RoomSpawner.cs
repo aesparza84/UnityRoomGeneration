7b2ea1eeec23801e8a3d70ac6f2e3bf498fd002b
aesparza84 <aesparza@colum.edu>
2023-09-17 14:55:38 -0500

    refining spawning

--- Project/ProceduralRoomsPOC/Assets/Scripts/RoomSpawner.cs
+++ Project/ProceduralRoomsPOC/Assets/Scripts/RoomSpawner.cs
@@ -8,47 +8,70 @@ public class RoomSpawner : MonoBehaviour
     public bool hasN, hasE, hasS, hasW;
 
     private RoomsToAdd possibleRooms;
+    public bool hasSpawnedRooms =false;
+    static int spawnedRooms = 0;
 
     void Start()
     {
         possibleRooms = GameObject.FindGameObjectWithTag("RoomManager").GetComponent<RoomsToAdd>();
-        checkOpoenings();
+        Invoke("checkOpoenings", 1.5f);
+        //checkOpoenings();
     }
 
 
     void Update()
     {
-        Debug.Log("Has N: " + hasN);
-        Debug.Log("Has E: " + hasE);
-        Debug.Log("Has S: " + hasS);
-        Debug.Log("Has W: " + hasW);
+        //Debug.Log("Has N: " + hasN);
+        //Debug.Log("Has E: " + hasE);
+        //Debug.Log("Has S: " + hasS);
+        //Debug.Log("Has W: " + hasW);
+        Debug.Log("Room count: "+spawnedRooms);
     }
 
     private void checkOpoenings()
     {
-        if (entryN != null)
+        if (spawnedRooms < 4 && !hasSpawnedRooms)
         {
-            hasN = true;
-            
-            GameObject n = Instantiate(possibleRooms.NorthRooms[getRandomElement(possibleRooms.NorthRooms)], entryN.position, Quaternion.identity);
+            if (entryN != null)
+            {
+                hasN = true;
+
+                GameObject n = Instantiate(possibleRooms.NorthRooms[getRandomElement(possibleRooms.NorthRooms)], entryN.position, Quaternion.identity);
+            }
+            if (entryE != null)
+            {
+                hasE = true;
+                GameObject n = Instantiate(possibleRooms.EastRooms[getRandomElement(possibleRooms.EastRooms)], entryE.position, Quaternion.identity);
+            }
+            if (entryS != null)
+            {
+                hasS = true;
+                GameObject n = Instantiate(possibleRooms.SouthRooms[getRandomElement(possibleRooms.SouthRooms)], entryS.position, Quaternion.identity);
+            }
+            if (entryW != null)
+            {
+                hasW = true;
+                GameObject n = Instantiate(possibleRooms.WestRooms[getRandomElement(possibleRooms.WestRooms)], entryW.position, Quaternion.identity);
+            }
+            spawnedRooms++;
+            hasSpawnedRooms = true;
         }
-        if (entryE != null)
-        {
-            hasE = true;
-        }
-        if (entryS != null)
-        {
-            hasS = true;
-        }
-        if (entryW != null)
-        {
-            hasW = true;
-        }
+        
     }
 
     private int getRandomElement(GameObject[] n)
     { 
         int r = Random.Range(0, n.Length);
         return r;
+    }
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        //Debug.Log(gameObject.name +" is hitting "+coll.name);
+        if (coll != null)
+        {
+            RoomSpawner s = coll.GetComponent<RoomSpawner>();
+            s.hasSpawnedRooms = true;
+        }
     }
 }
